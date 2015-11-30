060052324febf46357c22405009369c8ddb15306
japaro <joe.aparo@gmail.com>
2015-11-30 16:10:07 -0500

    Remove ITypeNature

--- jsa-shared/jsa-crib-ds/src/main/java/ind/jsa/crib/ds/api/ITypeNature.java
+++
@@ -1,9 +0,0 @@
-package ind.jsa.crib.ds.api;
-
-public interface ITypeNature {
-	public static final byte SCALAR = 0x01;
-	public static final byte INTEGRAL = 0x02;
-	public static final byte FRACTIONAL = 0x04;
-	public static final byte SEQUENCED = 0x08;
-	public static final byte ATOMIC = 0x10;
-}
