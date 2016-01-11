d3396c4682e805e8e73c452e2b0fcda9fd33e824
japaro <joe.aparo@gmail.com>
2016-01-11 09:30:53 -0500

    Add SqlDate converters

---
+++ jsa-shared/jsa-crib-ds/src/main/java/ind/jsa/crib/ds/internal/type/convert/bson/ToSqlDtUtils.java
@@ -0,0 +1,32 @@
+package ind.jsa.crib.ds.internal.type.convert.bson;
+
+import java.sql.Date;
+
+import org.bson.BsonDateTime;
+import org.bson.BsonInt32;
+import org.bson.BsonInt64;
+import org.bson.BsonString;
+
+public class ToSqlDtUtils {
+	private ToSqlDtUtils() {}
+	
+	public static Date bsonStr2Dt(BsonString val) {
+		return val != null ? 
+			ind.jsa.crib.ds.internal.type.convert.std.ToSqlDtUtils.str2SqlDt(((BsonString) val).toString()) : null;
+	}
+	
+	public static Date bsonInt2Dt(BsonInt32 val) {
+		return val != null ? 
+			ind.jsa.crib.ds.internal.type.convert.std.ToSqlDtUtils.lng2SqlDt(((BsonInt32) val).longValue()) : null;
+	}
+	
+	public static Date bsonLng2Dt(BsonInt64 val) {
+		return val != null ? 
+			ind.jsa.crib.ds.internal.type.convert.std.ToSqlDtUtils.lng2SqlDt(((BsonInt64) val).longValue()) : null;
+	}
+	
+	public static Date bsonDt2Dt(BsonDateTime val) {
+		return val != null ? 
+			ind.jsa.crib.ds.internal.type.convert.std.ToSqlDtUtils.lng2SqlDt(((BsonDateTime) val).getValue()) : null;
+	}
+}
---
+++ jsa-shared/jsa-crib-ds/src/main/java/ind/jsa/crib/ds/internal/type/convert/std/ToSqlDtUtils.java
@@ -0,0 +1,52 @@
+package ind.jsa.crib.ds.internal.type.convert.std;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.sql.Date;
+
+import net.jsa.common.logging.LogUtils;
+
+public class ToSqlDtUtils {
+	private ToSqlDtUtils() {
+	}
+
+	public static Date str2SqlDt(String val) {
+		Date ret = null;
+		String pattern = DateTimePattern.UNIVERSAL_DATETIME_PATTERN.toString();
+        SimpleDateFormat fmt = new SimpleDateFormat(pattern);
+        try {
+            ret = new Date(fmt.parse(val).getTime());
+        } catch (Exception ex) {
+			// Intentionally swallow, log, and return null
+			LogUtils.getLogger().warn("Unable to convert string " + val + " with format " + pattern + " to a date value");
+        }
+        return ret;
+	}
+
+	public static Date lng2SqlDt(Long val) {
+		return val != null ? new Date(val.longValue()) : null;
+	}
+
+	public static Date bgi2SqlDt(BigInteger val) {
+		return val != null ? new Date(val.longValue()) : null;
+	}
+
+	public static Date bgd2SqlDt(BigDecimal val) {
+		return val != null ? new Date(val.longValue()) : null;
+	}
+
+	public static Date cal2SqlDt(Calendar val) {
+		return val != null ? new Date(val.getTimeInMillis()) : null;
+	}
+
+	public static Date dt2SqlDt(java.util.Date val) {
+		return val != null ? new Date(val.getTime()) : null;
+	}
+	
+	public static Date ts2SqlDt(Timestamp val) {
+		return val != null ? new Date(val.getTime()) : null;
+	}
+}
